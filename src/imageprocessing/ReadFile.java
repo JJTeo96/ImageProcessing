07ebffd491ebd254e24d9745a499dd2d52456cc1
Student <Student@192.168.11.198>
2020-03-03 11:52:16 +0800

    Break new line

--- src/imageprocessing/ReadFile.java
+++ src/imageprocessing/ReadFile.java
@@ -5,28 +5,56 @@ public class ReadFile {
     public static void main(String[] args) {
         try{
             FileInputStream myInputFile = new FileInputStream("image/yoda.tif");
-            int value;
-            String byteOrder = null;
-            int index = 0;
+            int value,j;
+            j = 0;
+//            String byteOrder = null;
+//            int index = 0;
             while((value = myInputFile.read())!= -1){
+                
+                String hexFormat = String.format("%02X", value);
+                System.out.printf("%5s",hexFormat);
+                j++;
+                if (j == 10) {
+                    System.out.println("");
+                    j = 0;
+                }
+               
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
 //                String hexFormat = Integer.toHexString(value);
-                if (index == 0 && value == 73) {
-                    int value2 = myInputFile.read();
-                    if (value2 == 73) {
-                        byteOrder = "LSB";
-                        System.out.println("Byte Order: " + byteOrder);
-                    }
-                    index++;
-                } else if (index ==0 && value==0)
+//                if (index == 0 && value == 73) {
+//                    int value2 = myInputFile.read();
+//                    if (value2 == 73) {
+//                        byteOrder = "LSB";
+//                        System.out.println("Byte Order: " + byteOrder);
+//                    }
+//                    index++;
+//                } else if (index ==0 && value==0)
 //                String hexFormat = String.format("%02X", value);
 //                System.out.printf("%5s",hexFormat);
+//                j++;
+                
 //                System.out.printf("%5s", value);
 //			if(value % 5 == 0)
 //			{
 //				System.out.println();
 //			}
-                index++;
-
+//                index++;
+//
             }
 //            System.out.println(myInputFile.read());
 //            System.out.println(myInputFile.read());
